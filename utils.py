67891b126689623c702c88aef9e471638c9e60a1
musatov <shawolaaa@gmail.com>
2021-05-22 15:55:07 +0700

    refactor utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,6 @@
 from client import spotify_client
 
 
-def get_track_ids(items):
-    return [item['track']['id'] for item in items]
-
-
 def get_playlist_items(playlist_id):
     playlist_len = spotify_client.playlist(playlist_id)['tracks']['total']
     playlist_items = []
@@ -13,6 +9,29 @@ def get_playlist_items(playlist_id):
         playlist_items += spotify_client.playlist_items(playlist_id, offset=offset)['items']
 
     return playlist_items
+
+
+def get_track_ids(playlist_items):
+    return [item['track']['id'] for item in playlist_items]
+
+
+def get_playlist_track_ids(playlist_ids):
+    track_ids = []
+    for playlist_id in playlist_ids:
+        track_ids += get_track_ids(get_playlist_items(playlist_id))
+
+    return track_ids
+
+
+def add_tracks_to_playlist(playlist_id, track_ids):
+    for i in range(0, len(track_ids), 100):
+        spotify_client.playlist_add_items(playlist_id, track_ids[i: i + 100])
+
+
+def clear_playlist(playlist_id):
+    track_ids = get_track_ids(get_playlist_items(playlist_id))
+    for i in range(0, len(track_ids), 100):
+        spotify_client.playlist_remove_all_occurrences_of_items(playlist_id, track_ids[i: i + 100])
 
 
 def get_user_playlists(user_id):
@@ -31,34 +50,6 @@ def exclude(full_list, excluded_list):
     return [item for item in full_list if item not in excluded_list]
 
 
-def get_playlist_track_ids(playlist_ids):
-    track_ids = []
-    for playlist_id in playlist_ids:
-        track_ids += get_track_ids(get_playlist_items(playlist_id))
-
-    return track_ids
-
-
-def add_tracks_to_playlist(playlist_id, tracks):
-    for i in range(0, len(tracks), 100):
-        spotify_client.playlist_add_items(playlist_id, tracks[i: i + 100])
-
-
-def clear_playlist(playlist_id):
-    track_ids = get_track_ids(get_playlist_items(playlist_id))
-    for i in range(0, len(track_ids), 100):
-        spotify_client.playlist_remove_all_occurrences_of_items(playlist_id, track_ids[i: i + 100])
-
-
-def move_n_tracks_to_top(n, playlist_id):
-    track_ids = get_track_ids(get_playlist_items(playlist_id))
-    playlist_len = len(track_ids)
-    spotify_client.playlist_reorder_items(playlist_id,
-                                          range_start=playlist_len - n,
-                                          insert_before=0,
-                                          range_length=n)
-
-
 def playlist_name_to_id(user_id, playlist_name):
     user_playlists = get_user_playlists(user_id)
 
@@ -68,3 +59,12 @@ def playlist_name_to_id(user_id, playlist_name):
     name_to_id_dict = dict(zip(playlist_names, playlist_ids))
 
     return name_to_id_dict[playlist_name]
+
+
+def move_n_tracks_to_top(n, playlist_id):
+    track_ids = get_track_ids(get_playlist_items(playlist_id))
+    playlist_len = len(track_ids)
+    spotify_client.playlist_reorder_items(playlist_id,
+                                          range_start=playlist_len - n,
+                                          insert_before=0,
+                                          range_length=n)
