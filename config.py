52a3ec3aa4ae23a2523c280b0af76cbe3b2d48d9
Alexey M <53351191+iwsylit@users.noreply.github.com>
2021-05-22 13:29:28 +0700

    add move_playlist to config

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ config = dict(
     user_id='',
     lucky_playlist_id='',
     # do not forget to add the lucky playlist to the excluded list too
-    excluded_playlist_ids=['']
+    excluded_playlist_ids=[''],
+    move_playlist=''
 )
