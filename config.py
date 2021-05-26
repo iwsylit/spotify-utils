33f2e22e089b2eef144809150058edbc25ee00f3
musatov <shawolaaa@gmail.com>
2021-05-26 14:29:27 +0700

    update config and authorization

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 # excluded playlists - playlists that won't be added to the lucky playlist
 # do not forget to add the lucky playlist to the excluded list too
 
-config = dict(
-    user_id='',
-    default_lucky_playlist_name='',
-    excluded_playlist_names=[''],
-    default_move_playlist_name=''
-)
+user_id = ''
+lucky_playlist_name = ''
+excluded_playlist_names = ['']
+default_move_playlist_name = ''
